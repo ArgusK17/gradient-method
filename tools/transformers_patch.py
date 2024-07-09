bc0acd885049591c48e68a1f349b4d87458b35fc
Yifan Luo <luoyf@pku.edu.cn>
2024-07-09 22:19:23 +0800

    New Commit

--- tools/transformers_patch.py
+++ tools/transformers_patch.py
@@ -3,10 +3,8 @@ import warnings
 from typing import Optional, Tuple
 
 '''
-These are monkey patches for the transformers pack. 
-We modify forward() in DecoderLayer to allow hook attachment.
+We monkey patch forward method in the DecoderLayer to allow hook attachment.
 '''
-
 
 def forward_mod(
         self,
@@ -45,7 +43,7 @@ def forward_mod(
         # Self Attention
         hidden_states, self_attn_weights, present_key_value = self.self_attn(
             hidden_states,
-            attention_mask,
+            attention_mask, # MODIFIED HERE
             position_ids=position_ids,
             past_key_value=past_key_value,
             output_attentions=output_attentions,
@@ -70,62 +68,3 @@ def forward_mod(
             outputs += (present_key_value,)
 
         return outputs
-
-# def MistralDecoderLayer_forward_mod(
-#         self,
-#         hidden_states: torch.Tensor,
-#         attention_mask: Optional[torch.Tensor] = None,
-#         position_ids: Optional[torch.LongTensor] = None,
-#         past_key_value: Optional[Tuple[torch.Tensor]] = None,
-#         output_attentions: Optional[bool] = False,
-#         use_cache: Optional[bool] = False,
-#         **kwargs,
-#     ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
-#         if "padding_mask" in kwargs:
-#             warnings.warn(
-#                 "Passing `padding_mask` is deprecated and will be removed in v4.37. Please make sure use `attention_mask` instead.`"
-#             )
-#         """
-#         Args:
-#             hidden_states (`torch.FloatTensor`): input to the layer of shape `(batch, seq_len, embed_dim)`
-#             attention_mask (`torch.FloatTensor`, *optional*): attention mask of size
-#                 `(batch, sequence_length)` where padding elements are indicated by 0.
-#             output_attentions (`bool`, *optional*):
-#                 Whether or not to return the attentions tensors of all attention layers. See `attentions` under
-#                 returned tensors for more detail.
-#             use_cache (`bool`, *optional*):
-#                 If set to `True`, `past_key_values` key value states are returned and can be used to speed up decoding
-#                 (see `past_key_values`).
-#             past_key_value (`Tuple(torch.FloatTensor)`, *optional*): cached past key and value projection states
-#         """
-
-#         residual = hidden_states
-
-#         hidden_states = self.input_layernorm(hidden_states)
-
-#         # Self Attention
-#         hidden_states, self_attn_weights, present_key_value = self.self_attn(
-#             hidden_states,
-#             attention_mask,
-#             position_ids=position_ids,
-#             past_key_value=past_key_value,
-#             output_attentions=output_attentions,
-#             use_cache=use_cache,
-#         )
-#         hidden_states = residual + hidden_states
-
-#         # Fully Connected
-#         residual = hidden_states
-#         hidden_states = self.post_attention_layernorm(hidden_states)
-#         hidden_states = self.mlp(hidden_states)
-#         hidden_states = residual + hidden_states
-
-#         outputs = (hidden_states,)
-
-#         if output_attentions:
-#             outputs += (self_attn_weights,)
-
-#         if use_cache:
-#             outputs += (present_key_value,)
-
-#         return outputs
